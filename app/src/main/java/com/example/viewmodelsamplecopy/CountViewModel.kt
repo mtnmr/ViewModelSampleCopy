d90848f85b694c6873a700b3f9a7f57101887fe1
mtnmr <mei.rila1013@gmail.com>
2022-02-04 21:11:58 +0900

    dataBinding

--- app/src/main/java/com/example/viewmodelsamplecopy/CountViewModel.kt
+++ app/src/main/java/com/example/viewmodelsamplecopy/CountViewModel.kt
@@ -1,14 +1,24 @@
 package com.example.viewmodelsamplecopy
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CountViewModel: ViewModel() {
-    val counterB : MutableLiveData<Int> by lazy {
-        MutableLiveData<Int>()
-    }
 
-    init {
-        counterB.value = 0
+    private val _counterB:MutableLiveData<Int> = MutableLiveData<Int>(0)
+
+    val counterB:LiveData<Int> = _counterB
+
+//    val counterB : MutableLiveData<Int> by lazy {
+//        MutableLiveData<Int>()
+//    }
+//
+//    init {
+//        counterB.value = 0
+//    }
+
+    fun countUp(){
+        _counterB.value = _counterB.value!! + 1
     }
 }
--- app/src/main/java/com/example/viewmodelsamplecopy/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelsamplecopy/MainActivity.kt
@@ -4,34 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import com.example.viewmodelsamplecopy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        var counterA : Int = 0
+        val viewModel: CountViewModel by viewModels()
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.countModel = viewModel
+        binding.lifecycleOwner = this
 
-        val viewModel: CountViewModel= ViewModelProvider(this).get(CountViewModel::class.java)
 
+        var counterA: Int = 0
+
+//        val viewModel: CountViewModel= ViewModelProvider(this).get(CountViewModel::class.java)
+//        val viewModel: CountViewModel by viewModels()
 
         val text1 = findViewById<TextView>(R.id.text1)
-        val text2 = findViewById<TextView>(R.id.text2)
+//        val text2 = findViewById<TextView>(R.id.text2)
+
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            counterA ++
+            counterA++
             text1.text = counterA.toString()
 
-            viewModel.counterB.value = viewModel.counterB.value!! + 1
-            text2.text = viewModel.counterB.toString()
-
-            val countObserver = Observer<Int>{counter ->
-                text2.text = counter.toString()
-            }
-            viewModel.counterB.observe(this, countObserver)
+            viewModel.countUp()
+//            viewModel.counterB.value = viewModel.counterB.value!! + 1
         }
+
+//        val countObserver = Observer<Int>{counter ->
+//            text2.text = counter.toString()
+//        }
+//        viewModel.counterB.observe(this, countObserver)
+////    }
     }
 }
